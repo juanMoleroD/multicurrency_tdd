c510ada12cb71ae510096a58c3c94af810bc92be
juanMoleroD <juanldimeco@gmail.com>
2022-08-27 21:54:36 +0100

    16 - further implementation

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -17,4 +17,8 @@ class Sum implements Expression {
         return new Sum(this, addend);
     }
 
+    public Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
+    }
+
 }
